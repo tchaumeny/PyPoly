4d831af1ed9604b4357c57a49a1965b4186f734c
Luchau <chaumeny@outlook.com>
2013-12-25 21:26:13 +0100

    Small refactoring: do not use temporary variable

--- pypoly/examples.py
+++ pypoly/examples.py
@@ -16,10 +16,9 @@ def ChebyshevIterator():
     V = X
     yield V
     while True:
-        T = V
-        V = 2 * X * V - U
+        U, V = V, 2 * X * V - U
         yield V
-        U = T
+
 
 def Chebyshev(n):
     it = ChebyshevIterator()
@@ -38,11 +37,9 @@ def LegendreIterator():
     yield V
     n = 1
     while True:
-        T = V
-        V = 1. / (n + 1) * ((2 * n + 1) * X * V - n * U)
+        U, V = V, 1. / (n + 1) * ((2 * n + 1) * X * V - n * U)
         yield V
-        U = T
-        n += 1 
+        n += 1
 
 def Legendre(n):
     it = LegendreIterator()
